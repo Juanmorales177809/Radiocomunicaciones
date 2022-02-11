496fb7c67b73a066c50aeebecd56fda6cb847874
Carlos <mutaharmorales@gmail.com>
2022-02-11 15:36:27 -0500

    first commit

---
+++ convertir.py
@@ -0,0 +1,12 @@
+
+
+
+def ftmet(ft):
+    return ft/3.2808
+def megGig(meg):
+    return meg/1000
+
+
+if __name__ == "__main__":
+    print(ftmet(10))
+    print(megGig(917))
---
+++ presupuesto.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+
+class Presupuesto:
+    def __init__(self,f,D):
+        self.c = 3*10**8
+        self.f = f
+        self.wavLegh = self.f/self.c
+        self.D = D
+        self.A = 0
+        self.B = 0
+        self.R = 0.9999
+    #Perdidas en trayectoria por espacio libre (adimensional)
+    def lostFree(self,D):
+        return ((4*np.pi*D)/self.wavLegh)**2
+    #Perdidas en trayectoriaa por espacio libre (Frecuencia en Ghz y distancia en Km)
+    def lostFreegi(self,D):
+        return 92.4 +20*np.log10(self.f)+20*np.log10(D)
+    #Perdidas en trayectoriaa por espacio libre (Frecuencia en Mhz y distancia en Km)
+    def lostFreemeg(self):
+        return 32.4+20*np.log10(self.f)+20*np.log10(self.D)
+    
+    def factorRugo(self,val):
+        if val == "Agua o terreno liso":
+            self.A = 4
+        elif val == "Terreno Promedio":
+            self.A = 1
+        elif val == "Aspero y montanoso":
+            self.A = 0.25
+    def factorProba(self,prob):
+        if prob == "Anual a mensual":
+            self.B = 1
+        elif prob == "Areas calientes o humedas":
+            self.B = 0.5
+        elif prob ==  "Areas continentales promedio":
+            self.B = 0.25
+        elif prob == "Areas muy secas o montanosas":
+            self.B = 0.125
+    #Margen de desvanecimiento (decibeles)
+    def desvanecimiento(self,val,prob):
+        self.factorRugo(val)
+        self.factorProba(prob)
+        return 30*np.log10(self.D)+10*np.log10(6*self.A*self.B*self.f)-10*np.log10(1-self.R)-70
+
+    
+if __name__ == "__main__":
+    lost = Presupuesto(1.8,40)
+    d = lost.desvanecimiento("Agua o terreno liso","Areas calientes o humedas")
+    print(d, "dB")
+        
